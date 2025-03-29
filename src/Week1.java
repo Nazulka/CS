187d040d7e3be4ceb939f5a1540d613aaae9cce8
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-03-29 20:09:58 +0000

    Add week1 hw

---
+++ src/Week1.java
@@ -0,0 +1,20 @@
+
+    public class AverageCalculator {
+        public static void main(String[] args) {
+            System.out.println("Average finder");
+            double avg = findAverage(args);
+            System.out.println("The average is:");
+        }
+
+        static double findAverage(String[] input) {
+            double result = 0;
+            if (input.length ==0) {
+                return result;
+            }
+
+            for (String s: input) {
+                result += Integer.parseInt(s);
+            }
+            return result/ input.length;
+        }
+    }
